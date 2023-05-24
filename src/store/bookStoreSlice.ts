4265d26bd8f29edc0a67f049ad3da1206e4e12f0
krok-86 <8700478@mail.ru>
2023-05-24 18:03:33 +0300

    first step, create new project

---
+++ src/store/bookStoreSlice.ts
@@ -0,0 +1,11 @@
+// import { createSlice } from "@reduxjs/toolkit/dist/createSlice";
+
+// const bookStoreSlice = createSlice ({
+//     name: 'bookStore',
+//     initialState: {
+//         bookStore: []
+//     },
+//     redusers: {
+
+//     },
+// });
