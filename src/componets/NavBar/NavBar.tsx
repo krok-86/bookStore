a6543f6a2e6f9a3cd59b9f8200f350d9d1e1ce13
krok-86 <8700478@mail.ru>
2023-05-30 19:52:13 +0300

    NavBar

--- src/componets/NavBar/NavBar.tsx
+++ src/componets/NavBar/NavBar.tsx
@@ -3,24 +3,72 @@ import NavBarStyled from './NavBarStyled';
 
 const NavBar = () => {
 
-const navItemes = ["О Компании","Новости","Доставка","Оплата","Контакты","Статьи","Отзывы покупателей",]
+    const navItemes = ["О Компании", "Новости", "Доставка", "Оплата", "Контакты", "Статьи", "Отзывы покупателей",]
+
+
 
     return (
         <NavBarStyled>
             <nav className='NavButtons'>
-        {navItemes.map((item) => {
-    return (
-         <div>{item}</div>)
-})}
-</nav>
-<div className = "Info">
-<img className = "Logo" src = '/images/logo.png'/>
-<div className = "ShowRoom">Адрес шоу-рума (все товары в наличии):
-Москва, Большая Серпуховская, д. 46, стр. 34</div>
-<button className = "DrivingDirections">Как проехать или дойти?</button>
-<div className="WorkTime">Время работы:</div>
-<div className="Daily">Ежедневно c 10:00 до 21:00</div>
+                {navItemes.map((item) => {
+                    return (
+                        <div>{item}</div>)
+                })}
+            </nav>
+            <div className="Info">
+                <img className="Logo" src='/images/logo.png' />
+
+                <div className="AddressTitle">
+                    <div className="ShowRoom">Адрес шоу-рума (все товары в наличии):</div>
+                    <div className="ShowRoomAddress">Москва, Большая Серпуховская, д. 46, стр. 34</div>
+                    <button className="DrivingDirections">Как проехать или дойти?</button>
+                    <div className="WorkTime">Время работы:</div>
+                    <div className="Daily">Ежедневно c 10:00 до 21:00</div>
+                </div>
+
+                <div className="Phones">
+                    <div className="PhonesContacts">Контактные телефоны</div>
+                    <div className="PhonesCity">Москва: +7(495)999-99-99</div>
+                    <div className="PhonesCountry">Россия: 8(800)999-99-99(бесплатный)</div>
+                    <div className="PhonesMessengers">Месенджеры для звонков из других стран:</div>
+                    <div className="PhonesNumber">+7(926)999-99-99</div>
+                </div>
+
+                <div className="SocialMedia">
+                    <div className="SocialMediaTraking">Следите за нами в социальных сетях:</div>
+                    <div className="SocialMediaAll">
+                        <a href='https://www.youtube.com/@donfishka6772/videos'>
+                            <img className="YouTube" src='/images/YouTube.png' />
+                        </a>
+                        <a href='https://vk.com/feed'>
+                            <img className="VK" src='/images/VK.png' />
+                        </a>
+                        <a href='https://ok.ru/'>
+                            <img className="OK" src='/images/OK.png' />
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div className = "Products">
+<button className = "ProductsCatalog">Каталог товаров</button>
+<div>
+<button className = "ProductSearchButton">
+<input className = "ProductSearch" value={"Название или артикул товара"}></input>
+</button>
 </div>
+<div>
+    <button className = "ProductsFavorites">Избранное</button>
+    <div className = "ProductsFavoritesCounter"></div>
+</div>
+<div>
+    <button className = "ProductsBasket">Корзина</button>
+    <div className = "ProductsBasketCounter"></div>
+</div>
+<div className = "PersonalInformation">
+    <button className = "Personal">Личный кабинет</button>
+    <button className = "Registration">Регистрация</button>
+</div>
+            </div>
 
         </NavBarStyled>
     );
--- src/componets/NavBar/NavBarStyled.ts
+++ src/componets/NavBar/NavBarStyled.ts
@@ -2,8 +2,8 @@ import styled from 'styled-components';
 
 const NavBarStyled = styled.div`
 max-width: 1440px;
-/* background-color: EAEAEA;  */
 width: 100%;
+
 .NavButtons {
     display: flex;
 column-gap: 15px;
@@ -11,23 +11,152 @@ justify-content: flex-end;
 padding: 15px 30px 15px 0;
     color: #bdcdd5;
 }
-.Logo {
-   width : 150px;
-   height: 112px;
-}
 .Info {
     display: flex;
     width: 100%;
-background-color: #404040;
+    background-color: #222e3d;
+}
+.Logo {
+    display: flex;
+    width: 150px;
+    height: 112px;
+    padding: 5px;
+}
+.AddressTitle {
+display:flex;
+flex-direction: column;
+justify-content: space-between;
+padding: 5px;
+}
+.ShowRoom {
+    color: #d3d3d3;
+    font-size: 13px;
+}
+.ShowRoomAddress {
+    color: #ffffff;
+    font-size: 13px;
 }
 .DrivingDirections {
     display: flex;
+    align-items: center;
+    justify-content: center;
     background-color: #4e6481;
     border-radius: 99px;
     border: 0px;
     width: 188px;
     height: 27px;
     text-align: center;
+    color: #ffffff;
+    font-size: 13px;
+}
+.WorkTime  {
+    color: #ffffff;
+    font-size: 13px;
+}
+.Daily {
+    color: #ffffff;
+    font-size: 13px;
+}
+
+.Phones {
+    display:flex;
+flex-direction: column;
+justify-content: space-between;
+padding: 5px;
+}
+.PhonesContacts {
+    color: #d3d3d3;
+    font-size: 13px
+}
+.PhonesCity {
+    color: #ffffff;
+    font-size: 13px;
+}
+.PhonesCountry {
+    color: #ffffff;
+    font-size: 13px;
+}
+.PhonesMessengers {
+    color: #ffffff;
+    font-size: 13px;
+}
+.PhonesNumber {
+    color: #ffffff;
+    font-size: 13px;
+}
+
+.SocialMedia {
+    display:flex;
+flex-direction: column;
+justify-content: space-between;
+padding: 5px;
+}
+.SocialMediaTraking {
+    color: #d3d3d3d3;
+    font-size: 13px;
+}
+.SocialMediaAll {
+
+}
+
+.YouTube {
+    background-color: #ff0000;
+    width: 26px;
+    border-radius: 99px;
+    margin: 5px;
+}
+.VK {
+    background-color:#4a76a8;
+    width: 26px;
+    border-radius: 99px;
+    margin: 5px;
+}
+.OK {
+    background-color:#ee8208;
+    width: 26px;
+    border-radius: 99px;
+    margin: 5px;
+}
+
+.Products {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    background-color: #2d3f56;
+    padding: 15px;
+}
+.ProductsCatalog {
+    width: 175px;
+    font-size: 15px;
+    color: #ffffff;
+    padding: 12px 3px 12px 45px;
+    border-radius: 99px;
+    border: 0px;
+    text-align: center;
+    background: #4e6481;
+}
+.ProductsCatalog:hover {
+background-color: #ff0000;
+}
+.ProductSearch {
+    width: 175px;
+    font-size: 15px;
+    color: gray;
+    padding: 12px 35px 12px 20px;
+    border-radius: 99px;
+    border: 0px;
+    text-align: start;
+    background: #ffffff;
+}
+.ProductSearchButton {
+    width: 195px;
+    font-size: 15px;
+    color: gray;
+    padding: 12px 35px 12px 20px;
+    border-radius: 99px;
+    border: 0px;
+    text-align: start;
+    background: #4e6481;
 }
 `
 export default NavBarStyled;
