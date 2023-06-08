4b1181829b99e50baaf547b0660339c240306dd3
krok-86 <8700478@mail.ru>
2023-06-08 12:00:10 +0300

    footer

--- src/componets/FirstPage/Footer/Footer.tsx
+++ src/componets/FirstPage/Footer/Footer.tsx
@@ -32,29 +32,38 @@ const mailing = [
     return (
         <FooterStyled>
 <p className="Warning">© 2005-2023. Рыболовный интернет-магазин «Японские снасти». Копирование материалов без разрешения правообладателя запрещено.</p>
+<div className="CommonBlock">
+<div className="LeftBlock">
 <div className="Card">
 <p>Мы принимаем онлайн и в шоу-руме:</p>
-<div>
+<div className="CardItem">
     {cards.map((item)=>{
         return (
         <div className={item.class}>{item.value}</div>
     )})}
     </div>  
     </div>
-<div>Мы отправляем заказы службами:</div>
-<div>
+<div className="Delivery">Мы отправляем заказы службами:</div>
+<div className="Mail">
     {mailing.map((item)=>{
         return (
             <div className={item.class}>{item.value}</div>
         )
     })}
 </div>
-<p><b>ИП Иванов Иван Иванович</b></p>
-<p><b>ИНН 8888888888888</b></p>
-<div>
+<div className="Entity">
+<p>ИП Иванов Иван Иванович</p>
+<p>ИНН 8888888888888</p>
+</div>
+</div>
+
+<div className="CenterBlock">
+<div className="Documentation">
     <p>Политика конфиденциальности</p>
     <p>Пользовательское соглашение</p>
     <p>Гарантия и возврат</p>
+</div>
+</div>
 </div>
 
         </FooterStyled>
--- src/componets/FirstPage/Footer/FooterStyled.ts
+++ src/componets/FirstPage/Footer/FooterStyled.ts
@@ -4,21 +4,75 @@ const FooterStyled = styled.div`
 max-width: 1440px;
 width: 100%;
 background-color: #2d3f56;
+
+
 .Warning {
     color: #ffffff;
-    margin: 30px ;
+    padding: 30px ;
+}
+.CommonBlock {
+    max-width: 700px;
+    display: flex;
+    justify-content: space-between;
+
+}
+.LeftBlock {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
 }
 .Card {
     color: #d3d3d3;
 }
+.CardItem {
+    display: flex;
+    justify-content: space-between;
+    max-width: 250px;
+}
 .Visa {
-color: blue;
+    font-size: 22px;
+    font-weight: 900;
+    color: #075aa2;    
 }
 .MasterCard {
-    color: red;
+    font-size: 22px;
+    font-weight: 900;
+    color: #c72131;
 }
 .Mir {
-    color: green;
+    font-size: 22px;
+    font-weight: 900;
+    color: #22a64d;
+}
+.Delivery {
+    color: #d3d3d3;    
+}
+.Mail {
+    display: flex;
+    justify-content: space-between;
+    max-width: 228px;
+}
+.Post {
+    font-size: 22px;
+    font-weight: 900;
+    color: #1937ff;
+}
+.InstantMail {
+    color: #ed8f1d;
+    font-size: 22px;
+    font-weight: 900;
+}
+
+.DeliveryService {
+    font-size: 22px;
+    font-weight: 900;
+    color: #07ac3c;
+}
+.Entity {
+    color: #d3d3d3;
+}
+.Documentation{
+    color: #d3d3d3;
 }
 `
 
